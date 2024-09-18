f766b39d9d6620097de2b0609e4e7335e902ee57
Zioun <jionkhan0@gmail.com>
2024-09-18 07:53:14 +0600

    first commit

---
+++ main.css
@@ -0,0 +1,10 @@
+// back to top
+const toTop = document.querySelector('.to-top');
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset >2000 ){
+        toTop.classList.add('active');
+    }else{
+        toTop.classList.remove('active');
+    }
+})
